4f6acd7d4b218955d17ee0b0978b2c109190f669
Painnha <124910657+Painnha@users.noreply.github.com>
2024-05-23 03:16:37 +0700

    Update DepartmentDAO.java

--- school_server/src/main/java/dao/DepartmentDAO.java
+++ school_server/src/main/java/dao/DepartmentDAO.java
@@ -8,4 +8,7 @@ import entities.Department;
 public interface DepartmentDAO {
 	// Count Course by Department
 	public Map<Department, Long> countCoursesByDepartment();
+	public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    List<Department> findBudgetGreaterThan(@Param("budget") double budget, @Param("startDate") LocalDateTime startDate);
+}
 }
